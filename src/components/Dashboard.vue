2f6572377bf168d9e9374db7523a99f044063bd1
Janet Karpenske <janetkarpenske@gmail.com>
2024-10-21 14:30:43 -0700

    Dynamically render tabs based off the pet information returned from database

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -9,22 +9,30 @@
             bg-color="rgb(38, 39, 48)"
             align-tabs="center"
         >
-            <v-tab value="one">Maple</v-tab>
-            <v-tab value="two">Tigger</v-tab>
-            <v-tab value="three">Marley</v-tab>
+            <v-tab v-for="pet in userPets"
+            :key="pet.upName"
+            :value="pet.key">{{ pet.upName }}</v-tab>
+            <!-- <v-tab value="two">Tigger</v-tab>
+            <v-tab value="three">Marley</v-tab> -->
         </v-tabs>
 
         <v-card-text>
             <v-tabs-window v-model="tab">
-                <v-tabs-window-item value="one">
-                    <pet-details />
+                <v-tabs-window-item 
+                    v-for="pet in userPets"
+                    :value="pet.key"
+                    :key="pet.upName"
+                    >
+                    <pet-details 
+                        :petDetails="pet"
+                    />
                 </v-tabs-window-item>
-                <v-tabs-window-item value="two">
+                <!-- <v-tabs-window-item value="two">
                     <pet-details />
                 </v-tabs-window-item>
                 <v-tabs-window-item value="three">
                     <pet-details />
-                </v-tabs-window-item>
+                </v-tabs-window-item> -->
             </v-tabs-window>
         </v-card-text>
         </v-card>
@@ -33,13 +41,44 @@
 
 <script>
 import PetDetails from '@/components/PetDetails.vue';
+import { userStore } from '@/store/userStore';
+import { DB } from '@/firebase/config';
+import { collection, query, where, getDocs } from 'firebase/firestore'
 
+//options API
 export default {
   components: { PetDetails },
     data: () => ({
       tab: null,
+      items: [],
+      userPets: []
     }),
-  }
+    methods: {
+        async getUserPets(userID) {
+        const q = query(collection(DB, "userPets"), where("upUserID", "==", userID));
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot?.forEach((doc) => {
+
+        const pet = {
+            upName: doc.data().upName,
+            upSpecies: doc.data().upSpecies,
+            upUserID: doc.data().upUserID,
+            upOwner: doc.data().upOwner,
+            upSex: doc.data().upSex
+        };
+        this.userPets.push(pet);
+        });
+        }
+    },
+    created() {
+        const userStoreRef = userStore();
+        let user = userStoreRef.getUser;
+        if(user != null) {
+            this.getUserPets(user.uid);
+        }
+    }
+  };
 </script>
 
 <style scoped>
--- src/components/PetDetails.vue
+++ src/components/PetDetails.vue
@@ -1,9 +1,16 @@
 <template>
-    Name: Hazel
-    <br>
-    Breed: Dog
+    <div>
+        Name: {{ petDetails.upName }}
+        <br>
+        Species: {{ petDetails.upSpecies }}
+        <br>
+        Sex: {{ petDetails.upSex }}
+        <br>
+        Owner Email/Username: {{ petDetails.upOwner }}
+
+    </div>
 </template>
 
 <script setup>
-
+    const props = defineProps(['petDetails'])
 </script>
