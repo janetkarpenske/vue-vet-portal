b45dc5e07650db21a631e028bfd759ac47e28d39
Janet Karpenske <janetkarpenske@gmail.com>
2024-10-25 16:35:50 -0700

    Add functionality to load full pet details when one is selected

--- src/components/views/FullPetDetails.vue
+++ src/components/views/FullPetDetails.vue
@@ -1,12 +1,38 @@
 <template>
-    <h1> Pet Full Details</h1>
+    <h1>Pet Full Details</h1>
 </template>
 
 <script setup>
+    import { reactive, ref } from 'vue';
     import { useRoute } from 'vue-router';
+    import { doc, getDoc } from "firebase/firestore";
+    import { onBeforeMount, onMounted } from 'vue';
+    import { DB } from '@/firebase/config';
 
     const route = useRoute();
-    console.log(route.params);
+    const petData = reactive({
+    pet:''
+  });
+
+    onMounted(()=>{
+        getPetInfo();
+    })
+
+const getPetInfo = async() => {
+try{
+    const docRef = doc(DB, "userPets", route.params.petid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        petData.pet = docSnap.data();
+    } else {
+    alert("No such pet exists");
+    }
+}
+catch(error){
+    console.log(error);
+}
+  }
 </script>
 
 <style scoped>
