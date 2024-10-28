324c48079d7b1863edd0bd094b2f350cf8ea2693
Janet Karpenske <janetkarpenske@gmail.com>
2024-10-28 16:23:31 -0700

    Add functionality to delete a pet entry and redirect to user dashboard

--- src/components/views/FullPetDetails.vue
+++ src/components/views/FullPetDetails.vue
@@ -1,17 +1,32 @@
 <template>
-    <h1>Pet Full Details</h1>
+    <h1>{{ petData.pet.upName }}'s Details</h1>
+    <b>Name</b> {{ petData.pet.upName }}
+    <br>
+    <b>Species</b> {{ petData.pet.upSpecies }}
+    <br>
+    <b>Sex</b> {{ petData.pet.upSex }}
+    <br>
+    <b>Owner Email/Username</b> {{ petData.pet.upOwner }}
+    <br>
+    <v-btn
+        @click="handleDeletePet"
+    >
+        Delete Pet
+    </v-btn>
 </template>
 
 <script setup>
     import { reactive, ref } from 'vue';
-    import { useRoute } from 'vue-router';
-    import { doc, getDoc } from "firebase/firestore";
+    import { useRoute, useRouter } from 'vue-router';
+    import { doc, getDoc, deleteDoc } from "firebase/firestore";
     import { onBeforeMount, onMounted } from 'vue';
     import { DB } from '@/firebase/config';
 
     const route = useRoute();
+    const router = useRouter();
     const petData = reactive({
-    pet:''
+    pet:'',
+    petid: ''
   });
 
     onMounted(()=>{
@@ -19,20 +34,29 @@
     })
 
 const getPetInfo = async() => {
-try{
-    const docRef = doc(DB, "userPets", route.params.petid);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-        petData.pet = docSnap.data();
-    } else {
-    alert("No such pet exists");
+    try{
+        const docRef = doc(DB, "userPets", route.params.petid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            petData.pet = docSnap.data();
+            petData.petid = route.params.petid;
+        } else {
+            alert("No such pet exists");
+        }
+    }
+    catch(error) {
+        console.log(error);
     }
 }
-catch(error){
-    console.log(error);
+const handleDeletePet = async() => {
+    try{
+        await deleteDoc(doc(DB, "userPets", petData.petid));
+        router.push('/dashboard');
+    }
+    catch(error) {
+        console.log(error);
+    }
 }
-  }
 </script>
 
 <style scoped>
