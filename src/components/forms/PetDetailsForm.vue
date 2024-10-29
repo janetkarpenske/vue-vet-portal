9fefa90a47089c54e215241beed8ed339c040263
Janet Karpenske <janetkarpenske@gmail.com>
2024-10-29 10:35:18 -0700

    Break out addEdit pet modal into separate child component and emit events when needed

--- src/components/forms/PetDetailsForm.vue
+++ src/components/forms/PetDetailsForm.vue
@@ -1,22 +1,167 @@
 <template>
-  <h1 class="header">Pet Name Here</h1>
-  <v-form v-model="valid">
-    <v-container width="500">
-      <v-text-field
-        v-model="firstname"
-        :counter="10"
-        :rules="nameRules"
-        label="Pet Name"
-        hide-details
-        required
-        clearable
-      ></v-text-field>
-      <br>
-    </v-container>
-  </v-form>
+<div class="text-center pa-4">
+    <v-dialog
+        v-model="isModalOpen"
+        width="auto"
+    >
+      <v-card
+        width="60vw"
+        prepend-icon="mdi-update"
+        text=""
+        align="center"
+        title="Register a Pet"
+      >
+      <v-card-text>
+        <v-row dense>
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-text-field
+                v-model="data.addPetFormData.petName"
+                label="Pet name*"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-text-field
+                v-model="data.addPetFormData.additionalNames"
+                hint="Add any nicknames or alternate names"
+                label="Additional Names"
+              ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col
+              cols="12"
+              sm="4"
+            >
+              <v-text-field
+                v-model="data.addPetFormData.age"
+                label="Age*"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="4"
+            >
+              <v-text-field
+                v-model="data.addPetFormData.sex"
+                label="Sex*"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="4"
+            >
+              <v-autocomplete
+                v-model="data.addPetFormData.species"
+                :items="['Canine','Feline','Equine','Misc']"
+                label="Species*"
+                auto-select-first
+                required
+              ></v-autocomplete>
+            </v-col>
+          </v-row>
+        <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-text-field
+                v-model="data.addPetFormData.breed"
+                label="Breed*"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <v-text-field
+                v-model="data.addPetFormData.color"
+                label="Color*"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <small class="text-caption text-medium-emphasis">*indicates required field</small>
+      </v-card-text>
+      <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            text="Close"
+            color="black"
+            variant="plain"
+            @click="emitCloseModal"
+          ></v-btn>
+
+          <v-btn
+            color="rgb(54, 56, 80)"
+            text="Save"
+            variant="tonal"
+            @click="handleAddPet"
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script setup>
+    import { reactive, ref, computed } from 'vue';
+    import { DB } from '@/firebase/config';
+    import { userStore } from '@/store/userStore';
+    import { collection, query, where, getDocs, doc, setDoc, addDoc } from 'firebase/firestore'
+
+    const emit = defineEmits(['modal-closed','pet-added']);
+    const props = defineProps(['isAddPetModalOpen','user']);
+    const isModalOpen = computed(() => props.isAddPetModalOpen)
+
+    const data = reactive({
+    pet:'',
+    petid: '',
+    addPetFormData: {
+      petName:'',
+      additionalNames:'',
+      sex:'',
+      age:'',
+      species:'',
+      breed:'',
+      color:''
+    }
+  });
+
+const emitCloseModal = () => {
+    emit('modal-closed');
+  }
+const handleAddPet = async() => {
+            await addDoc(collection(DB, "userPets"), {
+            upBreed: data.addPetFormData.breed,
+            upName: data.addPetFormData.petName,
+            upOwner: props.user.email,
+            upSex: data.addPetFormData.sex,
+            upSpecies: data.addPetFormData.species,
+            upUserID: props.user.uid
+            });
+            resetAddPetFormData();
+            //emit that a pet was added - close modal and reload pets in parent component
+            emit('pet-added');
+  }
+  const resetAddPetFormData = () => {
+    console.log("Reached", data.addPetFormData)
+            data.addPetFormData.breed = "";
+            data.addPetFormData.petName = "";
+            data.addPetFormData.species = "";
+            data.addPetFormData.additionalNames = "";
+            data.addPetFormData.color = "";
+  }
 
 </script>
 
--- src/components/views/Dashboard.vue
+++ src/components/views/Dashboard.vue
@@ -29,130 +29,24 @@
         </v-card-text>
         </v-card>
     </v-container>
-    <div class="text-center pa-4">
-    <v-dialog
-        v-model="isAddPetModalOpen"
-        width="auto"
-    >
-      <v-card
-        width="60vw"
-        prepend-icon="mdi-update"
-        text=""
-        align="center"
-        title="Register a Pet"
-      >
-      <v-card-text>
-        <v-row dense>
-            <v-col
-              cols="12"
-              sm="6"
-            >
-              <v-text-field
-                v-model="addPetFormData.petName"
-                label="Pet name*"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col
-              cols="12"
-              sm="6"
-            >
-              <v-text-field
-                v-model="addPetFormData.additionalNames"
-                hint="Add any nicknames or alternate names"
-                label="Additional Names"
-              ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col
-              cols="12"
-              sm="4"
-            >
-              <v-text-field
-                v-model="addPetFormData.age"
-                label="Age*"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col
-              cols="12"
-              sm="4"
-            >
-              <v-text-field
-                v-model="addPetFormData.sex"
-                label="Sex*"
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col
-              cols="12"
-              sm="4"
-            >
-              <v-autocomplete
-                v-model="addPetFormData.species"
-                :items="['Canine','Feline','Equine','Misc']"
-                label="Species*"
-                auto-select-first
-                required
-              ></v-autocomplete>
-            </v-col>
-          </v-row>
-        <v-row>
-            <v-col
-              cols="12"
-              sm="6"
-            >
-              <v-text-field
-                v-model="addPetFormData.breed"
-                label="Breed*"
-                required
-              ></v-text-field>
-            </v-col>
-
-            <v-col
-              cols="12"
-              sm="6"
-            >
-              <v-text-field
-                v-model="addPetFormData.color"
-                label="Color*"
-                required
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <small class="text-caption text-medium-emphasis">*indicates required field</small>
-      </v-card-text>
-      <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            text="Close"
-            color="black"
-            variant="plain"
-            @click="isAddPetModalOpen = false"
-          ></v-btn>
-
-          <v-btn
-            color="rgb(54, 56, 80)"
-            text="Save"
-            variant="tonal"
-            @click="handleAddPet"
-          ></v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
+    <pet-details-form
+      :isAddPetModalOpen="isAddPetModalOpen"
+      :user="user"
+      @modal-closed="isAddPetModalOpen = false"
+      @pet-added="handlePetRefresh"
+    />
 </template>
 
 <script>
 import PetDetails from '@/components/PetDetails.vue';
+import PetDetailsForm from '@/components/forms/PetDetailsForm.vue';
 import { userStore } from '@/store/userStore';
 import { DB } from '@/firebase/config';
 import { collection, query, where, getDocs, doc, setDoc, addDoc } from 'firebase/firestore'
 
 //options API
 export default {
-  components: { PetDetails },
+  components: { PetDetails, PetDetailsForm },
     data: () => ({
       tab: 0,
       items: [],
@@ -187,29 +81,10 @@ export default {
         });
         },
 
-        async handleAddPet(){
-            //Note: 3rd string param sets a custom id. Ommitting auto generates the id
-            await addDoc(collection(DB, "userPets"), {
-            upBreed: this.addPetFormData.breed,
-            upName: this.addPetFormData.petName,
-            upOwner: this.user.email,
-            upSex: this.addPetFormData.sex,
-            upSpecies: this.addPetFormData.species,
-            upUserID: this.user.uid
-            });
-            this.resetAddPetFormData();
+        async handlePetRefresh(){
             this.isAddPetModalOpen = false;
-            //clear and update our userPets
             this.userPets = [];
             await this.getUserPets(this.user.uid)
-        },
-
-        resetAddPetFormData(){
-            this.addPetFormData.breed = "";
-            this.addPetFormData.petName = "";
-            this.addPetFormData.species = "";
-            this.addPetFormData.additionalNames = "";
-            this.addPetFormData.color = "";
         }
     },
     created() {
